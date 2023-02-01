3987cf59adfab80c15ab71cc54aa2bf2db0bafe4
Ryan4253 <ryan.liao0305@gmail.com>
2023-02-02 00:56:33 +0800

    Added Constants File

--- Camera.py
+++ Camera.py
@@ -1,14 +1,15 @@
 import cv2
 import time
+import Constants
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(Constants.CAMERA_ID)
 
 def initialize():
-    cap.set(3,1920) # WIDTH
-    cap.set(4,1080) # HEIGHT
-    time.sleep(10)
+    cap.set(3, Constants.CAMERA_PIXEL_WIDTH) # WIDTH
+    cap.set(4, Constants.CAMERA_PIXEL_HEIGHT) # HEIGHT
+    time.sleep(Constants.CAMERA_INITIALIZE_TIME)
 
 def takePicture():
-    ret, frame = cap.read()
+    _, frame = cap.read()
     return frame
 

c15eef854e0852929cff65de56bcdd74538dd53b
Ryan4253 <ryan.liao0305@gmail.com>
2023-02-03 01:03:40 +0800

    Added bounding box detection

--- ImageManipulator.py
+++ ImageManipulator.py
@@ -14,3 +14,16 @@ def splitImage(image, row, col):
             cv2.imwrite(f"img{i}_{j}.png", piece)
 
     return ret
+
+def getBoundingBox(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+
+    th, binary = cv2.threshold(blurred , 0, 255, cv2.THRESH_OTSU)
+    contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(image, contours, -1, (0, 0, 0), 3)
+    
+    bounding_boxes = [cv2.boundingRect(cnt) for cnt in contours]
+    
+    return bounding_boxes
--- main.py
+++ main.py
@@ -3,10 +3,21 @@ import Detector
 import PriceCalculator
 import cv2
 import ImageManipulator
+import time
 
-Camera.initialize()
-images = ImageManipulator.splitImage(cv2.imread('img.jpg'), 2, 2)
-for image in images:
-    result = Detector.detectComponents(image, True)
-    print(result)
-    print(PriceCalculator.calculatePrice(result))
+# Camera.initialize()
+# images = ImageManipulator.splitImage(cv2.imread('images.jpg'), 4, 4)
+# for image in images:
+#     result = Detector.detectComponents(image, True)
+#     print(result)
+#     print(PriceCalculator.calculatePrice(result))
+
+image = cv2.imread('images.jpg')
+bounding_boxes = ImageManipulator.getBoundingBox(image)
+for bbox in bounding_boxes:
+    [x , y, w, h] = bbox
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    print(bbox)
+
+cv2.imshow("OUTPUT", image)
+cv2.waitKey(0)
